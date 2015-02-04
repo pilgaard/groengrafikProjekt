0056c8752f025361af7cc5c7448fcb05ef9b55b9
chrisjustesen <chrisjustesen@10.200.120.39>
2015-02-04 14:23:07 +0100

    Opret

---
+++ groengrafik/src/handler/OrdreHandler.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package handler;
+
+import java.sql.SQLException;
+import model.Ordre;
+
+
+/**
+ *
+ * @author chrisjustesen
+ */
+public class OrdreHandler {
+    
+    public static void OpretOrdre(Ordre ordre) throws SQLException {
+        Ordre NyOrdre = new Ordre(2, null, null);
+        
+        
+        
+        String sql = "insert into ProductRequest"
+                + "values('"+NyOrdre.getId()+"','"
+                + ""+NyOrdre.getLeveringsdato()+"','"
+                + ""+NyOrdre.getOprettelsesdato()+"''";
+        
+        DatabaseHandler.getInstance().execute(sql);
+    }
+    
+}
